aae5e6f681a9f676e30e6fcef8e42646801c3b82
Lolita Tarasova <LadyLolita26@yandex.ru>
2025-01-11 18:58:28 +0300

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,30 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const cardList = document.querySelector('.places__list');
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 function createCard(cardData) {
-  const cardNode = cardTemplate.cloneNode(true);
-  const cardImage = cardNode.querySelector('.card__image');
-  const cardTitle = cardNode.querySelector('.card__title');
-  const cardDeleteButton = cardNode.querySelector('.card__delete-button');
-
+  const cardNode = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardNode.querySelector(".card__image");
+  const cardTitle = cardNode.querySelector(".card__title");
+  const cardDeleteButton = cardNode.querySelector(".card__delete-button");
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
   cardTitle.textContent = cardData.name;
-  
-  cardDeleteButton.addEventListener('click', removeCard);
-
-  return cardNode; 
-} 
+  cardDeleteButton.addEventListener("click", () => removeCard(cardNode));
+  return cardNode;
+}
 
 // @todo: Функция удаления карточки
-function removeCard(e) {
-const cardElement = e.target.closest('.card');
-cardElement.remove();
+function removeCard(card) {
+  card.remove();
 }
 
 // @todo: Вывести карточки на страницу
 function loadCards(initialCards) {
-  initialCards.forEach(cards => {
+  initialCards.forEach((cards) => {
     const cardNode = createCard(cards);
     cardList.append(cardNode);
   });
